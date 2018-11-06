0550dc5fadacf3abeb21ac2c0cc48c931ad785ef
Joshua D. Forrest <joshuadforrest@gmail.com>
2018-11-06 16:27:31 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,17 @@
 #  learn spec/02_shoe_spec.rb
 
-class Shoe 
+class Shoe
   
-  attr_accessor :brand :color
-  
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
   def initialize(brand)
     @brand = brand
+  end
+
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
   end
   
 end
