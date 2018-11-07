bb2f85f4ffaef8fdcf4f639f884e58d0254f771d
kodyclemens <kody.clemens@gmail.com>
2018-11-07 16:56:28 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,26 @@
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
 
+  def initialize(title)
+    @title = title
+  end
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+  # Manually add some of the required readers and writers:
+
+  # def title
+  #   @title
+  # end
+
+  # def author=(author)
+  #   @author = author
+  # end
+
+  # def author
+  #   @author
+  # end
+end
