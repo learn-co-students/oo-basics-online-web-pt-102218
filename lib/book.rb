931a5217bfba8bfd611d8f7a86f807e6433c90cd
alexandercleasby <cleasby.alex@gmail.com>
2018-11-06 22:25:04 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
+    def initialize(title)
+        @title=title
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
 
+
+end
 
